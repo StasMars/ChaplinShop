6e549951cd9064b466cbe9857c15b69dcee1328c
Stas Mars <stasskywalker@gmail.com>
2024-01-18 18:01:46 +0300

    The fourth commit

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
