521684576492a9d6ae97b69e7e1e4fb50b388cd2
Stas Mars <stasskywalker@gmail.com>
2024-01-24 22:26:15 +0300

    Slugs for categories

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from goods.models import Products
 
-def catalog(request):
-
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug = category_slug))
 
     context = {
         "title": "Chaplin - Каталог",
