42de5ff68245c3b287e9f6b3e9ca9b22a7d311d2
Stas Mars <stasskywalker@gmail.com>
2024-01-25 18:59:49 +0300

    Add product filters

--- goods/templatetags/goods_tag.py
+++ goods/templatetags/goods_tag.py
@@ -1,8 +1,16 @@
 from goods.models import Categories
 from django import template
+from django.utils.http import urlencode
+
 
 register = template.Library()
 
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
