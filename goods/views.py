ae0e0a3e2c3c3bceebe5c2860724268fa92629eb
Stas Mars <stasskywalker@gmail.com>
2024-01-26 18:08:54 +0300

    Add search function

--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,21 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
+from goods.utils import q_search
 from goods.models import Products
 
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)   
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug = category_slug))
 
