5dbcc945123e28dfdb758bf17e9ef46060f70caf
Stas Mars <stasskywalker@gmail.com>
2024-01-29 17:14:30 +0300

    Add full text search and Postgresql connection

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -6,12 +7,16 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
-    keywords = [word for word in query.split() if len(word.lower()) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_objects = Q()
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word.lower()) > 2]
+
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    # return Products.objects.filter(q_objects)
