ae0db5dda49136a8b8458d06a48b86caa91d2e6f
Stas Mars <stasskywalker@gmail.com>
2024-01-29 17:57:23 +0300

    Improve text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,10 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 from goods.models import Products
 
@@ -10,13 +15,28 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
-    # keywords = [word for word in query.split() if len(word.lower()) > 2]
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span",
+        )
+    )
 
-    # q_objects = Q()
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span",
+        )
+    )
 
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token)
-    #     q_objects |= Q(name__icontains=token)
-    # return Products.objects.filter(q_objects)
+    return result
