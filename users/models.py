f46bc08383cb4f0c43c4bc78146dd862be4e7ecb
Stas Mars <stasskywalker@gmail.com>
2024-01-31 15:37:09 +0300

    Fix login form and controller

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser 
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+    
+
