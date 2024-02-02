1b580594412ccbf71587f8afd0858590b461f21d
Stas Mars <stasskywalker@gmail.com>
2024-02-02 17:49:06 +0300

    add "delete cart" button in carts

--- carts/views.py
+++ carts/views.py
@@ -21,6 +21,10 @@ def cart_add(request, product_slug):
 def cart_change(request, product_slug):
     pass
 
-def cart_remove(request, product_slug):
-    pass
+def cart_remove(request, cart_id):
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    
+    return redirect(request.META['HTTP_REFERER'])
 
