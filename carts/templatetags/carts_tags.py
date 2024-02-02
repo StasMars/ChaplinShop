cd6abeab1dc8adee29e0ae123ccde5df14236942
Stas Mars <stasskywalker@gmail.com>
2024-02-02 19:15:54 +0300

    Add AJAX request and JSON response

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,11 +1,10 @@
 from django import template
 from carts.models import Cart
-
+from carts.utils import get_user_carts
 
 register = template.Library()
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
+    return get_user_carts(request)
     
--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import redirect, render
 from carts.models import Cart
 from goods.models import Products
+from django.http import JsonResponse
+from carts.utils import get_user_carts
+from django.template.loader import render_to_string
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -16,7 +21,18 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {"carts": user_cart}, request=request
+    )
+
+    response_data = {
+        "message": 'Товар добавлен в корзину',
+        "cart_items_html": cart_items_html
+    }
+
+    return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
     pass
