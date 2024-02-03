a5929cc632f9b25ea394275baefa3ceed72a9091
Stas Mars <stasskywalker@gmail.com>
2024-02-03 16:10:52 +0300

    Add removing from cart AJAX and JSON response

--- carts/views.py
+++ carts/views.py
@@ -37,10 +37,23 @@ def cart_add(request):
 def cart_change(request, product_slug):
     pass
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
 
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    
-    return redirect(request.META['HTTP_REFERER'])
 
+    user_cart = get_user_carts(request)
+    cart_item_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+
+    response_data = {
+        "message": 'Товар удален',
+        "cart_items_html": cart_item_html,
+        "quantity_deleted": quantity
+    }
+
+    return JsonResponse(response_data)
